d864608ceefb391e4bebdb5f87901b4b2f33220d
Ademar <ademardeleonvazquez@gmail.com>
2023-11-09 18:19:05 -0600

    The first class has some content

--- src/components/classcontent/index.jsx
+++ src/components/classcontent/index.jsx
@@ -15,14 +15,22 @@ const ClassContent = () => {
       },[activeClass])
 
     return (
-        <div className='flex flex-col items-center absolute right-0 class-content'>
-            <h2 className='text-center text-5xl py-6 px-2 font-medium'>{classes[activeClass].title}</h2>
-            <div className=' text-lg'>
-                <p>Have not watched the lesson? Watch it <a href='https://www.3blue1brown.com/topics/linear-algebra' className=' text-blue-500'>here</a>.</p>
-            </div>
-            <div>
-                {classes[activeClass].math}
-            </div>
+        <div className='flex justify-center absolute right-0 pb-10 class-content-container'>
+            <div className='flex flex-col gap-5 items-center px-4 class-content'>
+                <h2 className='text-center text-5xl py-6 px-2 font-medium'>{classes[activeClass].title}</h2>
+                <div className=' text-lg'>
+                    <p>Have not watched the lesson? Watch it <a href='https://www.3blue1brown.com/topics/linear-algebra' className=' text-blue-500'>here</a>.</p>
+                </div>
+                <section className=' text-xl'>
+                    <p>{classes[activeClass].preview}</p>
+                    {classes[activeClass].exercises?.map((exercise) => (
+                        <>
+                            <h3 className='my-5 text-3xl font-medium text-center'>{exercise.title}</h3>
+                            <p>{exercise.description}</p>
+                        </>
+                    ))}
+                </section>
+            </div>            
         </div>
     );
 };
--- src/context/index.jsx
+++ src/context/index.jsx
@@ -6,7 +6,17 @@ const ClassesContext = createContext();
 const classes = [
     {
         title: 'Vectors, what even are they?',
-        math: 'When \\(a \\ne 0\\), there exists two solutions for\\(ax^2 + bx + c = 0\\) as \\[x = {-b \\pm \\sqrt{b^2-4ac} \\over 2a}.\\]',
+        preview: 'Vectors can be interpreted geometrically like arrows pointing somewhere to a coordinate plane. We write vectors vertically and with square brackets around them like this: \\begin{bmatrix}a \\\\c \\end{bmatrix}',
+        exercises: [
+            {
+                title: 'Vector Addition',
+                description: 'To add two vectors you just have to add the entries. For example, to add the vectors: \\[\\begin{bmatrix}1 \\\\0 \\end{bmatrix} \\begin{bmatrix}0 \\\\1 \\end{bmatrix}\\]we would do something like this: \\[\\begin{bmatrix}1 & + & 0 \\\\0 & + & 1 \\end{bmatrix} = \\begin{bmatrix}1 \\\\1 \\end{bmatrix}\\]Note that we cannot add vectors with different number of entries, for example: \\(\\begin{bmatrix}1 \\\\1 \\end{bmatrix}\\) and \\(\\begin{bmatrix}1 \\\\1 \\\\1 \\end{bmatrix}\\).',
+            },
+            {
+                title: 'Vector Multiplication by Scalars.',
+                description: 'In linear algebra, real numbers are called scalars. If you multiply a vector by a scalar, you are streching out that vector, so that its length n times larger than the original length. \\[n \\cdot \\begin{bmatrix}1 \\\\1 \\end{bmatrix}\\]You can also shorten their lengths by multiplying them by a fraction. \\[ \\frac{1}{2}\\cdot\\begin{bmatrix}1 \\\\1 \\end{bmatrix} = \\begin{bmatrix}\\frac{1}{2} \\\\ \\frac{1}{2} \\end{bmatrix}\\]',
+            },
+        ],
     },
     {
         title:  'Linear combinations, span, and basis vectors',
